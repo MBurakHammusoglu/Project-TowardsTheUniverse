80cf0d8ea246337580a84b0e2357c7cde7c15c62
Burak Hammusoglu <m.burakhammusoglu@gmail.com>
2023-05-19 13:45:12 +0300

    Movement process

--- Towards The Moon/Assets/Scripts/Movement.cs
+++ Towards The Moon/Assets/Scripts/Movement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float upSpeedValue=100;
+    [SerializeField] float rotationSpeedValue=100;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    
+    }
+    void ProcessThrust(){
+        if(Input.GetKey(KeyCode.Space)){
+            Debug.Log("Pressed Space");
+            rb.AddRelativeForce(Vector3.up * upSpeedValue * Time.deltaTime);
+        }
+    }
+    void ProcessRotation(){
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeedValue);
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            Debug.Log("Rotating right");
+            ApplyRotation(-rotationSpeedValue);
+        }
+
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation=false;
     }
 }
